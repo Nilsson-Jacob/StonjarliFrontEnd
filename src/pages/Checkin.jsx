e49c44d896d46e83e0137803d4b22d1094133cd4
nilsson-jacob <jacobnilsson97@gmail.com>
2025-12-26 15:19:59 +0100

    max

--- src/pages/Checkin.jsx
+++ src/pages/Checkin.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { MeshDistortMaterial, OrbitControls } from "@react-three/drei";
 
