f95b3721a8652c79c8694cb1f19996abc2e0ad5e
nilsson-jacob <jacobnilsson97@gmail.com>
2025-12-30 18:00:50 +0100

    max

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../components/supabaseClient";
 
 const Colors = {
   primary500: "#72063c",
