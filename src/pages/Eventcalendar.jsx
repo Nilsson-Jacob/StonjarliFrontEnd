68320a1aea96b2208258f06d7fdad0e1b5e2ce5e
nilsson-jacob <jacobnilsson97@gmail.com>
2026-04-02 10:59:04 +0200

    NEW V for Cafe Demo

--- src/pages/Eventcalendar.jsx
+++ src/pages/Eventcalendar.jsx
@@ -90,7 +90,7 @@ export default function Home() {
               })
             }
             style={{
-              width: "95%",
+              width: "93%",
               height: DAY_BOX_SIZE,
               background: "grey",
               padding: 8,
