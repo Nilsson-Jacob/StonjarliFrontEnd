3f45777816d4bcf0a5a6302dc482d6fd0d35a2f0
nilsson-jacob <jacobnilsson97@gmail.com>
2026-04-02 17:27:06 +0200

    NEW V for Cafe Demo

--- src/pages/Eventcalendar.jsx
+++ src/pages/Eventcalendar.jsx
@@ -165,7 +165,6 @@ export default function Home() {
             <span style={{ fontSize: 12, fontWeight: 600 }}>
               {formattedDate}
             </span>
-            {entry?.title && <div>{entry.title}</div>}
           </motion.div>
         );
 
@@ -414,6 +413,36 @@ export default function Home() {
                 overflowY: "auto",
               }}
             >
+              <h3>{selectedDay.fullDate}</h3>
+
+              {selectedDay.entry ? (
+                <>
+                  <h4 style={{ marginTop: 12 }}>Training</h4>(
+                  {selectedDay.entry.title ? (
+                    selectedDay.entry.title.map((a, i) => {
+                      return (
+                        <div
+                          key={i}
+                          style={{
+                            background: "rgba(0,0,0,0.25)",
+                            borderRadius: 12,
+                            padding: 10,
+                            marginBottom: 8,
+                          }}
+                        >
+                          <strong>{a.title}</strong>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <p style={{ opacity: 0.7 }}>No training logged.</p>
+                  )}
+                  <p style={{ opacity: 0.7 }}>No training logged.</p>)
+                </>
+              ) : (
+                <p>No data for this day.</p>
+              )}
+
               <button
                 onClick={() => setSelectedDay(null)}
                 style={{
