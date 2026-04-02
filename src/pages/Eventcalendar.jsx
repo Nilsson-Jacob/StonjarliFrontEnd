e2e920a6b250322a21a00573c14c9c4221c69203
nilsson-jacob <jacobnilsson97@gmail.com>
2026-04-02 10:03:18 +0200

    NEW V for Cafe Demo

--- src/pages/Eventcalendar.jsx
+++ src/pages/Eventcalendar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { supabase } from "../components/supabaseClient";
 import {
