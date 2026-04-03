058146be6761b1d931dd979cbb353cc88d140fe2
nilsson-jacob <jacobnilsson97@gmail.com>
2026-04-03 09:20:23 +0200

    NEW V for Cafe Demo

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -3,5 +3,13 @@ import { useParams } from "react-router-dom";
 export default function Home() {
   const { eventId } = useParams();
   console.log("here: " + eventId);
-  return <div>Hej {eventId.toString()}</div>;
+
+  return (
+    <div>
+      <div style={{ height: 200, background: "blue", alignItems: "center" }}>
+        <h2>Bageri Baka</h2>
+      </div>
+      Hej {eventId.toString()}
+    </div>
+  );
 }
