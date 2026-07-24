74d8f531287a47671a6738ec402dfbe815becec7
nilsson-jacob <jacobnilsson97@gmail.com>
2026-07-24 22:32:06 +0200

    logs

--- src/pages/Logs.jsx
+++ src/pages/Logs.jsx
@@ -222,7 +222,7 @@ export default function Logs() {
         color: Colors.text,
         padding: 12,
         paddingTop: 60,
-        overflowX: auto,
+        overflowX: "auto",
       }}
     >
       <h2 style={{ marginBottom: 12 }}>{format(currentMonth, "MMMM yyyy")}</h2>
