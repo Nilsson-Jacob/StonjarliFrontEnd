68fc354f139dac2f05c36d03bef777db9c4c6358
nilsson-jacob <jacobnilsson97@gmail.com>
2026-07-24 16:54:20 +0200

    logs

--- src/pages/Logs.jsx
+++ src/pages/Logs.jsx
@@ -171,7 +171,7 @@ export default function Logs() {
                 right: 6,
                 display: "flex",
                 gap: 6,
-                fontSize: 35,
+                fontSize: 27,
                 alignItems: "center",
               }}
             >
