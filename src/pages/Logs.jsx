3eeb1e26418f458710b0316f993dc2332f755eab
nilsson-jacob <jacobnilsson97@gmail.com>
2026-08-23 10:05:52 +0200

    logs from restet

--- src/pages/Logs.jsx
+++ src/pages/Logs.jsx
@@ -326,10 +326,37 @@ export default function Logs() {
                               marginBottom: 8,
                             }}
                           >
-                            <strong>{a.activity_type} 🏃</strong>
+                            <strong>
+                              {a.activity_type
+                                ? a.activity_type
+                                : a.training_type}{" "}
+                              🏃
+                            </strong>
                             <div style={{ fontSize: 13 }}>
                               {a.anchor_metric?.cardio}
                             </div>
+                            <div
+                              style={{
+                                fontSize: 12,
+                                marginTop: 4,
+                                color: "#7CFFB2",
+                              }}
+                            ></div>
+                          </div>
+                        );
+                      } else if (a.training_type === "sport") {
+                        return (
+                          <div
+                            key={i}
+                            style={{
+                              background: "rgba(0,0,0,0.25)",
+                              borderRadius: 12,
+                              padding: 10,
+                              marginBottom: 8,
+                            }}
+                          >
+                            <strong>{a.activity_type} 🏃</strong>
+                            <div style={{ fontSize: 13 }}>{a.notes}</div>
                             <div
                               style={{
                                 fontSize: 12,
